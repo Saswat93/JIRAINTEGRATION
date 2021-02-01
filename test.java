9a320e14f0554a685d7df548c3ec5cc6dd92b772
Saswat Pattnaik <saswatpattnaik21@gmail.com>
2021-02-01 19:20:31 +0530

    MERC-11

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+  public static void main(String[] args) {
+     System.out.print("Helloworld");
+<<<<<<< HEAD
+    System.out.print("Hi!! world Tapas"");
+                     System.out.print("GIT PULL DEMO");
+=======
+    System.out.print("Hi!! world Tapas HELLOWORLD"");
+>>>>>>> hellowrold added after tapas
+  }
+}
